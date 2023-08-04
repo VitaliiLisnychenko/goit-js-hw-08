01ba42edc98daf898fb1883ed9d52474630f5429
VitaliiLisnychenko <lisnychenkovitalii@gmail.com>
2023-08-04 20:11:31 +0200

    3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const FEEDBACK_FORM_KEY = "feedback-form-state";
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+let messageForm = JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY));
+const { email, message } = formEl.elements;
+loadForm();
+
+function onFormInput(){
+    messageForm = { email: email.value, message: message.value};
+    localStorage.setItem(FEEDBACK_FORM_KEY, JSON.stringify(messageForm));
+}
+
+function onFormSubmit(event){
+    event.preventDefault();
+    console.log({email: email.value, message: message.value});
+    localStorage.removeItem(FEEDBACK_FORM_KEY);
+    event.currentTarget.reset();
+}
+
+function loadForm(){
+    if(messageForm){
+       email.value = messageForm.email || "";
+       message.value = messageForm.message || "";
+    }
+}
